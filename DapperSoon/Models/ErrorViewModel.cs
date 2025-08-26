630dcb778bf4043e9186c35d010b46c1ea07c926
Erkut ÇAKAR <erkutcakar@gmail.com>
2025-08-26 23:06:44 +0300

    Add project files.

---
+++ DapperSoon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DapperSoon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
